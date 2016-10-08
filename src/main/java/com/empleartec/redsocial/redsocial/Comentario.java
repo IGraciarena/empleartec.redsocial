be1fbe3b91ef72f2a36607c412ede915790a4617
ivanmdq22 <ivangraciarena@outlook.com>
2016-10-08 12:54:17 -0300

    2

---
+++ src/main/java/com/empleartec/redsocial/redsocial/Comentario.java
@@ -0,0 +1,21 @@
+package com.empleartec.redsocial.redsocial;
+
+public class Comentario {
+	
+	private long ID;
+	private String txt;
+	
+	public String getTxt() {
+		return txt;
+	}
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	
+}
--- src/main/java/com/empleartec/redsocial/redsocial/Usuario.java
+++ src/main/java/com/empleartec/redsocial/redsocial/Usuario.java
@@ -1,3 +1,4 @@
+package com.empleartec.redsocial.redsocial;
 
 public class Usuario {
 	
@@ -6,5 +7,37 @@ public class Usuario {
 	private String nombre;
 	private int edad;
 	private Muro muroUsuario;
+	
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Muro getMuroUsuario() {
+		return muroUsuario;
+	}
+	public void setMuroUsuario(Muro muroUsuario) {
+		this.muroUsuario = muroUsuario;
+	}
+	
 	
 }
