24ad7c001a948447550fb62f79e4262e11d43df3
ivanmdq22 <ivangraciarena@outlook.com>
2016-10-15 12:50:21 -0300

    Add files via upload
    15/10/16

---
+++ empleartec.redsocial-master/src/main/java/com/empleartec/redsocial/redsocial/Comentario.java
@@ -0,0 +1,22 @@
+package com.empleartec.redsocial.redsocial;
+
+public class Comentario {
+	
+	private long idUser;
+	private String texto;
+	
+	public long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+
+}
---
+++ empleartec.redsocial-master/src/main/java/com/empleartec/redsocial/redsocial/Muro.java
@@ -0,0 +1,19 @@
+package com.empleartec.redsocial.redsocial;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Muro {
+	
+	private List<Publicacion> publicaciones = new ArrayList<Publicacion>();
+
+	public List<Publicacion> getPublicaciones() {
+		return publicaciones;
+	}
+
+	public void setPublicaciones(List<Publicacion> publicaciones) {
+		this.publicaciones = publicaciones;
+	}
+	
+	
+}
